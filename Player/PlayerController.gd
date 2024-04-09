48cf6e54ecafa35fd2b9fc3922bd27581f7fee2d
ldubouchet <ldubouchet@wanadev.fr>
2024-04-09 13:46:06 +0200

    fixed playercontroller and final case warnings and errors

--- Player/PlayerController.gd
+++ Player/PlayerController.gd
@@ -1,21 +1,27 @@
 extends CharacterBody3D
 
-
+@onready var headPivot: Node3D = $HeadPivot
 const SPEED = 1000.0
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-
-func _physics_process(delta):
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func _input(event):
+	if (event is InputEventMouseMotion):
+		headPivot.rotate_y(event.relative.x * -0.001)
+	
+func _handleMovement(delta): 
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y -= gravity * delta
+		self.velocity.y -= gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		self.velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -23,14 +29,18 @@ func _physics_process(delta):
 	var vertical_input = Input.get_action_strength("player_move_up") - Input.get_action_strength("player_move_down")
 	var horizontal_input = Input.get_action_strength("player_move_right") - Input.get_action_strength("player_move_left")
 	var moveVector = Vector3(horizontal_input, 0, -vertical_input).normalized()
-	var direction = (transform.basis * moveVector).normalized()
+	var direction = (headPivot.transform.basis * moveVector).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED * delta
-		velocity.z = direction.z * SPEED * delta
+		self.velocity.x = direction.x * SPEED * delta
+		self.velocity.z = direction.z * SPEED * delta
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED * delta)
-		velocity.z = move_toward(velocity.z, 0, SPEED * delta)
+		self.velocity.x = move_toward(velocity.x, 0, SPEED * delta)
+		self.velocity.z = move_toward(velocity.z, 0, SPEED * delta)
 	print(velocity)
-	var collider = move_and_slide()
+	var collider = self.move_and_slide()
+
+func _physics_process(delta):
+	self._handleMovement(delta)
+	
 	#if (collider):
 	#print(velocity)
