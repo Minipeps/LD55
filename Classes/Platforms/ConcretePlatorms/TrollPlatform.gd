e7d87a5becd7f846597f7965ab04602deb995346
Vynrae <ghaith.elghaddaf@gmail.com>
2024-04-13 18:04:53 +0200

    Snap corrigé

--- Classes/Platforms/ConcretePlatorms/TrollPlatform.gd
+++ Classes/Platforms/ConcretePlatorms/TrollPlatform.gd
@@ -6,21 +6,28 @@ var isPlayerOnTop = false
 
 func _ready():
 	originalPosition = position
+	
+func _process(delta):
+	print(isPlayerOnTop)
 
 func snap(direction):
-	position = originalPosition + direction
-
-func _on_player_area_detector_right_body_entered(body):
 	if(!isSnapped && !isPlayerOnTop):
-		snap(Vector3(-1,0,0))
+		position = originalPosition + direction
 		isSnapped = true
 		$SnapDuration.start()
-
-func _on_player_area_detector_left_body_entered(body):
-	snap(Vector3(1,0,0))
-	isSnapped = true
-	$SnapDuration.start()
-
+	
 func _on_snap_duration_timeout():
 	isSnapped = false
 	position = originalPosition
+
+func _on_player_area_detector_right_body_entered(body):
+	snap(Vector3(-1,0,0))
+
+func _on_player_area_detector_left_body_entered(body):
+	snap(Vector3(1,0,0))
+
+func _on_player_area_detector_top_body_entered(body):
+	isPlayerOnTop = true
+
+func _on_player_area_detector_top_body_exited(body):
+	isPlayerOnTop = false
