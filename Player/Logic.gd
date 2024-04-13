c15fd1573fbde5d0370acecb4a6fdb2a6c830089
unknown <lucas@dubouchet.eu>
2024-04-13 13:39:21 +0200

    sphere cursor

--- Player/Logic.gd
+++ Player/Logic.gd
@@ -3,10 +3,12 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-
+const PLATFORM_RANGE = 3
+var dropPlane  = Plane(Vector3(0, 0, 1), Vector3.AXIS_Z)
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
+@onready var camera = $Camera3D
+@onready var spherePivot = $SpherePivot
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -29,3 +31,18 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	
+	_handleSpawnSphere()
+	
+func _handleSpawnSphere():
+	var position2D = get_viewport().get_mouse_position()
+	var position3D = dropPlane.intersects_ray(camera.project_ray_origin(position2D), camera.project_ray_normal(position2D))
+	var directionVector = position3D - global_position
+	var currentVectorLength = directionVector.length()
+	var finalVectorLength = min(PLATFORM_RANGE, currentVectorLength)
+	var finalVector = directionVector.normalized() * finalVectorLength
+	finalVector.z = 0
+	spherePivot.global_position = global_position + finalVector
+	
+	
+	
