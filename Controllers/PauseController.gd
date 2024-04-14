5546dbb910246933da9818ef799c2b8b4fb925a4
Minipeps <mpcaudan@gmail.com>
2024-04-14 18:07:35 +0200

    add pause menu

--- Controllers/PauseController.gd
+++ Controllers/PauseController.gd
@@ -1,19 +1,22 @@
 extends Node
 
-var pauseMenu
+@onready var pauseMenu = $PauseMenu
+@onready var resumeButton = $PauseMenu/ResumeButton
+@onready var quitButton = $PauseMenu/QuitButton
+@onready var restartButton = $PauseMenu/RestartButton
 
 func _ready():
 	self._initialization()
-	
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	self._inputsHandler()
-	
+
 func _initialization():
-	pauseMenu = get_node("PauseMenu")
-	var resumeButton = get_node("PauseMenu/ResumeButton")
-	resumeButton.pressed.connect(self._onResumeButtonPressed) #event needs to be connected that way because resumeButton is in another node
-		
+	resumeButton.pressed.connect(self._onResumeButtonPressed) #event needs to dbe connected that way because resumeButton is in another node
+	quitButton.pressed.connect(self._onQuitButtonPressed)
+	restartButton.pressed.connect(self._onRestartButtonPressed)
+
 func _inputsHandler():
 	self._handlePauseInput()
 
@@ -31,3 +34,10 @@ func _handlePauseInput():
 func _onResumeButtonPressed():
 	get_node("/root").get_child(0).get_tree().paused = false
 	pauseMenu.visible = false
+
+func _onQuitButtonPressed():
+	get_tree().quit()
+
+func _onRestartButtonPressed():
+	get_node("/root").get_child(0).get_tree().paused = false
+	get_tree().reload_current_scene()
