6ee3a9d101d9ab3c536ff88c11eda8001d4ae26a
unknown <lucas@dubouchet.eu>
2024-04-14 17:38:23 +0200

    NextLevelPoitn now emits sound

--- Levels/LevelManager/NextLevelPoint.gd
+++ Levels/LevelManager/NextLevelPoint.gd
@@ -1,7 +1,10 @@
 extends Node3D
 
 @export var nextSceneFile : String
+@onready var sound = $AudioStreamPlayer3D
 
 func _on_area_3d_body_entered(body):
 	if body.is_in_group("Player") && !nextSceneFile.is_empty():
+		sound.play()
+		await get_tree().create_timer(2.0).timeout
 		get_tree().change_scene_to_file(nextSceneFile)
--- Player/Logic.gd
+++ Player/Logic.gd
@@ -15,6 +15,7 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var cursor = $Cursor
 @onready var jumpSounds = $"../Sounds/SonsSaut/AudioStreamPlayer3D"
 @onready var deathSounds = $"../Sounds/SonsMorts/AudioStreamPlayer3D"
+@onready var walkSound: AudioStreamPlayer3D = $"../Sounds/WalkSound"
 
 @onready var visual = $Visual
 var health: int = 1;
@@ -49,7 +50,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		jumpSounds.play()
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("player_move_left", "player_move_right", "player_move_up", "player_move_down")
@@ -60,6 +61,11 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	if is_on_floor() && velocity.x !=0 && !(walkSound.playing):
+		walkSound.play()
+	if (not is_on_floor() || velocity.x == 0) && walkSound.playing:
+		walkSound.stop()
 	
 	_handleCursorPosition()
 	_handleAnimation()
